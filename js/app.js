c1e78e5aa7e2bfc3668396c4f7132d424e431602
Lee Hoyt <leehoyt@bellsouth.net>
2015-02-11 10:34:19 -0600

    Add readme and instructions popup

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-var hop = new Audio('audio/hop.mp3');
-var lives = 4;
+
 // Enemies our player must avoid
 var Enemy = function(x, y, sprite) {
     // Variables applied to each of our instances go here,
@@ -26,7 +25,7 @@ Enemy.prototype.update = function(dt) {
     var buglength;
     if (this.x > 625) {
         this.x = -200;
-        this.speed = Math.floor(Math.random() * 100 + 80); 
+        this.speed = Math.floor(Math.random() * 100 + 80);
         if(this.y === 145) {
             this.sprite = 'images/rock.png';
         } else {
@@ -53,7 +52,7 @@ Enemy.prototype.update = function(dt) {
         && this.x <= (player.x + 42)
         && player.y <= (this.y + 42)
         && this.y <= (player.y + 42)
-        
+
     ) {
         playerReset();
     } else if (
@@ -62,14 +61,14 @@ Enemy.prototype.update = function(dt) {
         && this.x <= (player.x + 42)
         && player.y <= (this.y + 25)
         && this.y <= (player.y + 25)
-        
+
     ) {
         player.x = this.x;
         if (this.x > 550) {
             playerReset();
         }
     }
-}
+};
 
 //enemyType function randomly determines enemy types
 var enemyTypes = function() {
@@ -81,12 +80,12 @@ var enemyTypes = function() {
     } else {
         return 'images/enemy-bug3.png';
     }
-};        
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
@@ -94,7 +93,7 @@ var Player = function() {
     this.sprite = 'images/char-boy.png';
     this.x = 251;
     this.y = 450;
-}
+};
 
 Player.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
@@ -105,21 +104,18 @@ Player.prototype.update = function(dt) {
         this.y > 115
         && this.y < 160
         && this.x !== rock.x
-        //&& rock.x - this.x > 45
     ) {
         playerReset();
     }
-}
+};
 
 // Draw the player on the screen, required method for game
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    //console.log(this.x);
-}
+};
 
 Player.prototype.handleInput = function(key) {
-    //hop.play();
-     switch(key) {
+    switch(key) {
         case 'left':
             this.sprite = 'images/boy-left.png';
             this.x = this.x - 51;
@@ -153,6 +149,7 @@ Player.prototype.handleInput = function(key) {
     }
 };
 
+//Reset player to initial position after collision
 var playerReset = function() {
     player.x = 251;
     player.y = 450;
@@ -161,7 +158,7 @@ var playerReset = function() {
 
 //Draw jewels in top row
 var Jewels = function(x, color) {
-    
+
     this.y = 63;
     this.x = x;
     switch (color) {
@@ -191,7 +188,7 @@ Jewels.prototype.update = function() {
             this.marker === 1
             && gemcount !== 2
             && gemcount !== 3
-            && this.y <= 425            
+            && this.y <= 425
             && player.y <= (this.y + 45)
             && this.y <= (player.y + 55)
             && player.x <= (this.x + 55)
@@ -204,7 +201,7 @@ Jewels.prototype.update = function() {
             this.marker === 2
             && gemcount !== 1
             && gemcount !==3
-            && this.y <= 425            
+            && this.y <= 425
             && player.y <= (this.y + 45)
             && this.y <= (player.y + 55)
             && player.x <= (this.x + 55)
@@ -217,7 +214,7 @@ Jewels.prototype.update = function() {
             this.marker === 3
             && gemcount !== 1
             && gemcount !== 2
-            && this.y <= 425            
+            && this.y <= 425
             && player.y <= (this.y + 45)
             && this.y <= (player.y + 55)
             && player.x <= (this.x + 55)
@@ -238,7 +235,7 @@ var Princess = function() {
     this.sprite = 'images/char-princess-girl-sad.png';
     this.x = 5;
     this.y = 450;
-}
+};
 
 Princess.prototype.update = function() {
     if (
@@ -250,42 +247,44 @@ Princess.prototype.update = function() {
     } else if (lives === 0) {
         gameLose();
     }
-}
+};
 
 // Draw the princess on the screen
 Princess.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     //console.log(this.x);
-}
+};
+
 var livesLeft = function(){
     lives = lives - 1;
-    
-}
+
+};
 
 livesLeft.prototype.render = function(){
     ctx.font='20px Comic Sans MS';
     ctx.fillStyle = 'yellow';
     ctx.fillText('Lives Remaining: ' + lives,415,525);
-}
+};
 
-var gameWin = function(dt) {
+//Game endings -win or lose- displayed
+var gameWin = function() {
     princess.x = 225;
     player.x = 285;
     player.y = 450;
-    for (enemy of allEnemies) {
-        enemy.sprite = 'images/heart.png';
+    for (var i = 0; i < enemiesLength; i++) {
+        allEnemies[i].sprite = 'images/heart.png';
     }
-}
+};
 
 var gameLose = function() {
     princess.x = 225;
     player.x = 265;
     player.y = 485;
     player.sprite = 'images/boy-dead.png';
-    for (enemy of allEnemies) {
-        enemy.sprite = 'images/broken-heart.png';
+    for (var i = 0; i < enemiesLength; i++) {
+        allEnemies[i].sprite = 'images/broken-heart.png';
     }
-}
+};
 
 // Now instantiate your objects.
 var enemy1 = new Enemy(25, 62, enemyTypes());
@@ -297,12 +296,26 @@ var jewel2 = new Jewels(275, 2);
 var jewel3 = new Jewels(477, 3);
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [enemy1, enemy2, enemy3, rock];
+var enemiesLength = allEnemies.length;
 // Place all jewel objects in an array called allJewels
 var allJewels = [jewel1, jewel2, jewel3];
 // Place the player object in a variable called player
 var player = new Player();
 var princess = new Princess();
 var numlives = new livesLeft();
+var lives = 3;
+var instructions =
+'INSTRUCTIONS FOR PLAYING THE GAME:' + '\n' +
+'1) USE ARROW KEYS TO MOVE PLAYER AROUND GAMEBOARD.' + '\n' +
+'2) AVOID ALL BUGS!' + '\n' +
+'3) AVOID WATER. JUMP ON THE ROCK TO CROSS OVER WATER.' + '\n' +
+'4) GATHER GEMS TO RETURN TO THE PRINCESS. ONLY ONE GEM MAY BE CARRIED AT A TIME.' + '\n' +
+'5) PLAYER BEGINS WITH 3 LIVES. ANY COLLISIONS WITH BUGS OR WATER WILL SUBTRACT 1 LIFE.' + '\n' +
+'6) GAME IS OVER WHEN ONE OF THE FOLLOWING OCCURS:' + '\n' +
+    'a) ALL GEMS ARE RETURNED TO THE PRINCESS' + '\n' +
+    'b) ALL LIVES HAVE BEEN USED';
+
+alert(instructions);
 
 
 // This listens for key presses and sends the keys to your
